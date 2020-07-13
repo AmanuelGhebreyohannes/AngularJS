460083970009038fbed27f32a048780f3536d769
Amanuel Ghebreyohannes Welde <100043378@kustar.ac.ae>
2020-07-14 03:38:47 +0400

    `Implemented'

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,35 +1,51 @@
 (function(){
 'use strict';
-angular.module('NameCalculator',[])
-.controller('NameCalculatorController',function($scope,$filter){
-  $scope.name="";
-  $scope.totalValue=0;
-  $scope.imageName=1;
-  console.log($scope.name);
-  $scope.displayNumeric=function(){
-    var totalNameValue=calculateNumericForString($scope.name);
-    $scope.totalValue=totalNameValue;
-  };
-  $scope.upper=function(){
-    var upCase=$filter('uppercase');
-    $scope.name=upCase($scope.name);
-  };
-  $scope.feedMe=function(){
-    if($scope.imageName==1)
-    $scope.imageName=2;
-    else $scope.imageName=1;
+angular.module('LunchCheck',[])
 
-  };
-});
+.controller('LunchCheckController',LunchCheckController);
+LunchCheckController.$inject=['$scope','$filter'];
+function LunchCheckController($scope,$filter){
+  $scope.items="";
+  $scope.message="";
+
+  console.log($scope.items);
+
+  /***********this function doesn't count empty items thus comma separated empty item like item1,,item2********/
+  $scope.displayMessage=function(){
+
+    //outputing the items to the console
+    console.log($scope.items);
+
+      var str=$scope.items;
+      var str1=str.split(',');
+
+      var strArray=[];
+      var j=0;
+      for(var i=0;i<str1.length;i++){
+        if(str1[i].trim()!="")
+        {
+          strArray[j]=str1[i];
+          j++;
+        }
+      }
+        //outputting the actual number of items ignoring empty items
+      console.log(strArray.length);
+
+      if($scope.items==""||$scope.items==null||strArray.length==0)
+          $scope.message="Please enter data first!";
+
+      else
+        {
+          if(strArray.length<=3)
+            $scope.message= "Enjoy!";
+          else if(strArray.length>3)
+            $scope.message="Too much!";
+        }
+
+    $scope.items="";
 
 
-function calculateNumericForString(string){
-  var totalStringValue=0;
-  for(var i=0;i<string.length;i++)
-  {
-    totalStringValue+=string.charCodeAt(i);
   }
-  return totalStringValue;
-}
 
-})();
+
+}})();
